77d2b74701248a72f0030aa962384ae974ae6fd4
Vai Srivastava <vaisriv@icloud.com>
2025-12-04 15:48:04 -0500

    feat(nix): add submission helper scripts

--- .nix/devshell.nix
+++ .nix/devshell.nix
@@ -12,12 +12,66 @@ perSystem.devshell.mkShell {
 
     commands = [
         # python helper
-        # {
-        #     name = "py";
-        #     category = "[python]";
-        #     help = "run submission python script";
-        #     command = "python ../code/submission.py"
-        # }
+        {
+            name = "py";
+            category = "[submission]";
+            help = "run submission python script";
+            command =
+                # bash
+                ''
+                    CYAN="\e[0;36m"
+                    NC="\e[0m"
+
+                    cd $(git rev-parse --show-toplevel)
+
+                    echo -e -n "$CYAN"
+                    echo -e "running python script for enae441-$(basename $(pwd)):$NC"
+                    python ./submission.py
+                '';
+        }
+
+        # latex helpers
+        {
+            name = "ltx";
+            category = "[submission]";
+            help = "compile submission latex doc";
+            command =
+                # bash
+                ''
+                    CYAN="\e[0;36m"
+                    NC="\e[0m"
+
+                    cd $(git rev-parse --show-toplevel)
+
+                    echo -e -n "$CYAN"
+                    echo -e "compiling latex document for enae441-$(basename $(pwd)):$NC"
+                    latexmk
+                '';
+        }
+        {
+            name = "ltxc";
+            category = "[submission]";
+            help = "clean-compile submission latex doc";
+            command =
+                # bash
+                ''
+                    BOLDCYAN="\e[1;36m"
+                    CYAN="\e[0;36m"
+                    NC="\e[0m"
+
+                    cd $(git rev-parse --show-toplevel)
+
+                    echo -e -n "$BOLDCYAN"
+                    echo -e "compiling latex document for enae441-$(basename $(pwd)):\n"
+                    echo -e -n "$CYAN"
+                    echo -e "removing old latex aux files:$NC"
+                    latexmk -C
+
+                    echo -e -n "$CYAN"
+                    echo -e "compiling latex document:$NC"
+                    latexmk
+                '';
+        }
     ];
 
     packages = with pkgs; [
