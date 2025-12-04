aa3fe9e0c06a023363220ba853f8885e7b3a4a18
Vai Srivastava <vaisriv@icloud.com>
2025-12-04 16:06:07 -0500

    feat(tex): add points per problem, example python text output, update titlepage format, update problem part format, and add line number to code hyperlink

--- src/hw00.py
+++ src/hw00.py
@@ -6,8 +6,15 @@ import os
 def main():
     print("Hello World!")
 
+    #######
+    # p02 #
+    #######
+
     x = np.linspace(0, 10, 100)
     y = np.sin(x)
+
+    with open("./outputs/text/s02.txt", "w", encoding="utf-8") as f:
+        f.write((f"x:\n{x}\n\ny:\n{y}"))
 
     plt.figure()
     plt.plot(x, y)
